0cdb90966914b058ac1daff3f1b6542790fd92af
Roman Viarbitski <5682065+rviar@users.noreply.github.com>
2019-01-16 16:24:24 +0300

    Add files via upload

--- grymala/js/script.js
+++ grymala/js/script.js
@@ -83,6 +83,7 @@
       for (var i = 0; i < videos.length; i++) {
         videos[i].addEventListener('loadedmetadata', function () {
           var i = parseInt(this.getAttribute('id').split('-').pop());
+          console.log(this);
 
           var button = document.createElement('button');
           button.setAttribute('id', 'play-button-' + i);
